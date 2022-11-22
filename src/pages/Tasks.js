b016d6eb296511e7f726cb0272d1d6f609bca9c9
Arash Cipriano Cardoso Kaffashi <arash_kaffashi@hotmail.com>
2022-11-22 02:38:22 -0400

    Task Page Implements: axios get & display

--- src/pages/Tasks.js
+++ src/pages/Tasks.js
@@ -1,10 +1,19 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { Table, Container, Button, Col, Row } from "react-bootstrap";
 import ModalTarefas from "../components/ModalTarefas";
 
 export default function Tasks() {
-  let [showModal, setShowModal] = useState(false);
+  const [tasks, setTasks] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      let response = await axios("https://ironrest.cyclic.app/gtr_task");
+      setTasks(response.data);
+    })();
+  }, []);
 
   function handleModal() {
     setShowModal(true);
@@ -30,12 +39,34 @@ export default function Tasks() {
               <th>Referência</th>
               <th>Membros</th>
               <th>Início</th>
-              <th>Horas Estimadas</th>
+              <th>Minutos Estimados</th>
               <th>Prazo Final</th>
               <th>Tags</th>
+              <th>Detalhes</th>
             </tr>
           </thead>
           <tbody>
+            {tasks.map((task) => (
+              <tr key={task._id}>
+                <td>{task.status}</td>
+                <td>{task.nome}</td>
+                <td>{task.prioridade}</td>
+                <td>{task.peridiciodade}</td>
+                <td>{task.Referencia}</td>
+                <td>{task.membros}</td>
+                <td>{task.inicio}</td>
+                <td>{task.tempoestimado}</td>
+                <td>{task.PrazoFinal}</td>
+                <td>{task.tags}</td>
+                <td>
+                  <Link to={`/tasks/${task._id}`}>
+                    <Button variant="outline-secondary" size="sm">
+                      Detalhes
+                    </Button>
+                  </Link>
+                </td>
+              </tr>
+            ))}
             {/* {users.map((user) => {
               return (
                 <tr key={user._id}>
