9abb404d319762f37a5602fb0e012935de721828
Anderson Rodrigues Moro <andersonrodriguesmoro@gmail.com>
2022-11-24 06:30:00 -0300

    .

--- src/components/ModalTarefasUsers.js
+++ src/components/ModalTarefasUsers.js
@@ -1,6 +1,15 @@
 import axios from "axios";
 import { useState } from "react";
-import { Badge, Button, Modal, Row, Col, Form } from "react-bootstrap";
+import {
+  Badge,
+  Button,
+  Modal,
+  Row,
+  Col,
+  Form,
+  FloatingLabel
+
+} from "react-bootstrap";
 import { toast } from "react-hot-toast";
 import CheckboxList from "./CheckboxList";
 import MembersCheckbox from "./MembersCheckbox.js";
@@ -17,25 +26,77 @@ function ModalTarefasUsers({
   allMembers,
   edit,
   atribuir
-
-
-
 }) {
   const [members, setMembers] = useState(currentMembers);
   const [form, setForm] = useState({ ...taskObject, ...formObj });
   const [validated] = useState(false);
-
-
-
-
+  const [showRepeticao, setShowRepeticao] = useState(false);
 
   function handleClose() {
     setShow(false);
   }
 
+  function callRepeticao() {
+    handleClose();
+    setShowRepeticao(true);
+  }
+
+  function handleRepeticao() {
+    setShowRepeticao(false);
+  }
+
+  /* CRUD */
   function handleSubmit(e) {
     e.preventDefault();
     e.stopPropagation();
+    setShowRepeticao(false);
+    if (formObj && Object.keys(formObj).length) handlePut();
+    else handlePost();
+    handleClose();
+  }
+
+  async function handlePost() {
+    let clone = { ...form };
+    try {
+      await axios.post("https://ironrest.cyclic.app/gtr_task/", clone);
+      toast.success("Tarega criada com sucesso! :D");
+      setReload(!reload);
+    } catch (error) {
+      setShow(true);
+      console.log(error);
+      toast.error("Algo deu errado. Tente novamente.");
+    }
+  }
+
+  async function handlePut() {
+    try {
+      const clone = { ...form };
+      delete clone._id;
+
+      await axios.put(
+        `https://ironrest.cyclic.app/gtr_task/${form._id}`,
+        clone
+      );
+
+      toast.success("Alterações salvas");
+      setReload(!reload);
+    } catch (error) {
+      setShow(true);
+      console.log(error);
+      toast.error("Algo deu errado. Tente novamente.");
+    }
+  }
+
+  async function handleDelete(e) {
+    try {
+      await axios.delete(`https://ironrest.cyclic.app/gtr_task/${form._id}`);
+      toast.success("Tarefa deletada com sucesso");
+      setReload(!reload);
+      setShow(false);
+    } catch (error) {
+      console.log(error);
+      toast.error("Algo deu errado ao deletar essa tarefa.");
+    }
   }
 
   function handleChange({ target }) {
@@ -58,19 +119,6 @@ function ModalTarefasUsers({
         value: selected.map((item) => "" + item.matricula),
       },
     });
-  }
-
-  async function handlePost() {
-    let clone = { ...form };
-    try {
-      await axios.post("https://ironrest.cyclic.app/gtr_task/", clone);
-      toast.success("Tarega criada com sucesso! :D");
-      setReload(!reload);
-      handleClose(); // fechar o modal
-    } catch (error) {
-      console.log(error);
-      toast.error("Algo deu errado. Tente novamente.");
-    }
   }
 
   function obrigatorio() {
@@ -111,140 +159,40 @@ function ModalTarefasUsers({
         </Col>
       </Row>
     )
-
-  }
-
-
-  async function handlePut() {
-    try {
-      const clone = { ...form };
-      delete clone._id;
-
-      await axios.put(
-        `https://ironrest.cyclic.app/gtr_task/${form._id}`,
-        clone
-      );
-
-      toast.success("Alterações salvas");
-      setReload(!reload);
-      setShow(false);
-    } catch (error) {
-      console.log(error);
-      toast.error("Algo deu errado. Tente novamente.");
-    }
-  }
-
-  async function handleDelete(e) {
-    try {
-      await axios.delete(`https://ironrest.cyclic.app/gtr_task/${form._id}`);
-      toast.success("Tarefa deletada com sucesso");
-      setReload(!reload);
-      setShow(false);
-    } catch (error) {
-      console.log(error);
-      toast.error("Algo deu errado ao deletar essa tarefa.");
-    }
   }
 
   function selecionar() {
-
     if (atribuir) {
       return ([allMembers.find(element => element.matricula === atribuir)])
     }
 
     return (allMembers)
   }
+  
 
 
   return (
     <div>
       <Modal show={show} onHide={handleClose} size="xl">
-        <Modal.Header closeButton>
-          <Modal.Title>Formulário de criação de tarefas</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          {/* FORMULÁRIO */}
-          <Form noValidate validated={validated} onSubmit={handleSubmit}>
-            <Row>
-              <Col>
-                <Form.Group className="mb-3">
-                  <Form.Label htmlFor="prioridade">Prioridade</Form.Label>
-                  <Form.Select
-                    id="prioridade"
-                    name="prioridade"
-                    aria-label="prioridade"
-                    value={form.prioridade}
-                    onChange={handleChange}>
-                    <option value="Baixo">Baixo</option>
-                    <option value="Médio">Médio</option>
-                    <option value="Alto">Alto</option>
-                  </Form.Select>
-                </Form.Group>
-              </Col>
-              <Col>
-                <Form.Group className="mb-3">
-                  <Form.Label>Periodicidade</Form.Label>
-                  <Row>
-                    <Col>
-                      <CheckboxList
-                        updateParent={handleCheckbox}
-                        selected={form.periodicidade}
-                        options={{
-                          name: "periodicidade",
-                          type: "radio",
-                          list: periodicity,
-                        }}
-                      />
-                    </Col>
-                  </Row>
-                  <Row>
-                    {form.periodicidade === "semanal" && (
-                      <Col>
-                        <CheckboxList
-                          updateParent={handleCheckbox}
-                          selected={form.detalhesPeriodicidade}
-                          options={{
-                            name: "detalhesPeriodicidade",
-                            type: "checkbox",
-                            list: week,
-                          }}
-                        />
-                      </Col>
-                    )}
-                    {form.periodicidade === "mensal" && (
-                      <Col>
-                        <Form.Group as={Row} className="mb-3">
-                          <Form.Label
-                            column
-                            sm="2"
-                            htmlFor="detalhesPeriodicidade">
-                            Dia
-                          </Form.Label>
-                          <Col sm="10">
-                            <Form.Control
-                              type="number"
-                              id="detalhesPeriodicidade"
-                              name="detalhesPeriodicidade"
-                              placeholder="Dia do mês"
-                              value={
-                                typeof form.detalhesPeriodicidade === "string"
-                                  ? form.detalhesPeriodicidade
-                                  : "1"
-                              }
-                              min="1"
-                              max="31"
-                              onChange={handleChange}
-                              autoFocus
-                            />
-                          </Col>
-                        </Form.Group>
-                      </Col>
-                    )}
-                  </Row>
-                </Form.Group>
-              </Col>
-            </Row>
-            <Row>
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+          <Modal.Header closeButton>
+            <FloatingLabel
+              controlId="nome"
+              label="Nome da tarefa"
+              className="flex-grow-1">
+              <Form.Control
+                type="text"
+                id="nome"
+                name="nome"
+                value={form.nome}
+                onChange={handleChange}
+                required
+                autoFocus
+              />
+            </FloatingLabel>
+          </Modal.Header>
+          <Modal.Body>
+                      <Row>
               <Col>
                 <MembersCheckbox
                   update={updateMember}
@@ -256,27 +204,6 @@ function ModalTarefasUsers({
                     {member.nome}
                   </Badge>
                 ))}
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <Form.Group className="mb-3">
-                  <Form.Label htmlFor="nome">Nome da Tarefa</Form.Label>
-                  <Form.Control
-                    type="text"
-                    id="nome"
-                    name="nome"
-                    placeholder="Insira o nome da tarefa"
-                    value={form.nome}
-                    required
-                    onChange={handleChange}
-                    autoFocus
-                  />
-                </Form.Group>
-              </Col>
-              <Col>
-                <Form.Label htmlFor="tags">Tags</Form.Label>
-                <Tags update={updateTags} selected={form.tags} />
               </Col>
             </Row>
             <Row>
@@ -310,74 +237,55 @@ function ModalTarefasUsers({
                 </Form.Group>
               </Col>
               <Col>
-                <Form.Group className="mb-3">
-                  <Form.Label htmlFor="inicio">Início</Form.Label>
-                  <Form.Control
-                    type="date"
-                    id="inicio"
-                    name="inicio"
-                    value={form.inicio}
-                    onChange={handleChange}
-                  />
-                </Form.Group>
+                <Form.Label htmlFor="tags">Tags</Form.Label>
+                <Tags update={updateTags} selected={form.tags} />
               </Col>
             </Row>
             <Row>
               <Col>
                 <Form.Group className="mb-3">
                   <Form.Label htmlFor="tempoestimado">
-                    Minutos Estimados
+                    Tempo estimado
                   </Form.Label>
                   <Form.Control
-                    type="number"
+                    type="time"
                     id="tempoestimado"
                     name="tempoestimado"
                     value={form.tempoestimado}
                     onChange={handleChange}
-                    min="5"
-                    placeholder="Tempo em minutos para concluir a tarefa"></Form.Control>
+                    min="00:00"
+                    max="08:00"
+                    placeholder="Tempo estimado para concluir a tarefa"></Form.Control>
                 </Form.Group>
               </Col>
-              <Col>
-                <Form.Group className="mb-3">
-                  <Form.Label htmlFor="prazoFinal">Prazo Final</Form.Label>
-                  <Form.Control
-                    type="date"
-                    id="prazoFinal"
-                    name="prazoFinal"
-                    value={form.prazoFinal}
-                    onChange={handleChange}
-                  />
-                  <Form.Control.Feedback type="invalid">
-                    A data do prazo final não pode uma pretérita.
-                  </Form.Control.Feedback>
-                </Form.Group>
-              </Col>
-            </Row>
-          </Form>
-
-        </Modal.Body>
+               </Row>
+          </Modal.Body>
+        </Form>
         <Modal.Footer>
-          {!Object.keys(formObj).length ? (
+          {Object.keys(formObj).length ? (
             <>
-              <Button variant="secondary" onClick={handleClose}>
-                Cancelar
+              <Button variant="primary" onClick={handleSubmit}>
+                Salvar
               </Button>
-              <Button variant="primary" onClick={handlePost}>
-                Adicionar tarefa
+              <Button variant="primary" onClick={() => handleSubmit}>
+        
+                Concluir Tarefa
               </Button>
             </>
           ) : (
             <>
-
-              <Button variant="primary" onClick={handlePut}>
-                Salvar
+              <Button variant="secondary" onClick={handleClose}>
+                Cancelar
+              </Button>
+              <Button variant="primary" onClick={handleSubmit}>
+                Adicionar tarefa
               </Button>
             </>
           )}
         </Modal.Footer>
+    </Modal>
 
-      </Modal>
+    
     </div>
   );
 }
